360f5fb5db9f3f0c88f7116ce9f9b1e7a98d29ea
Natalie Agus <natalieagus@Natalies-MacBook-Pro-2.local>
2020-02-04 20:46:38 +0800

    fixed bug on listdir on wsl

--- PA1/shellPrograms/shellListDir_code.c
+++ PA1/shellPrograms/shellListDir_code.c
@@ -3,25 +3,32 @@
 /*
 	List the items in the directory
 */
-int shellListDir_code(char** args)
-{    
-	char* token = strtok(args[1], SHELL_OPT_DELIM);
-    printf("Token is %s\n",token );
+int shellListDir_code(char **args)
+{
+    if (args[1] != NULL)
+    {
+        char *token = strtok(args[1], SHELL_OPT_DELIM);
+        printf("Token is %s\n", token);
 
-	if (token != NULL){
-		if (strcmp(token,"a")==0){
-			//call listdirall, 
-            //execvp still need the ./shellPrograms because this was called
-            //by a process that was at the .. directory
-			if(execvp("./shellPrograms/listdirall", args) == -1){
-                perror("Failed to execute, command is invalid.");
+        if (token != NULL)
+        {
+            if (strcmp(token, "a") == 0)
+            {
+                //call listdirall,
+                //execvp still need the ./shellPrograms because this was called
+                //by a process that was at the .. directory
+                if (execvp("./shellPrograms/listdirall", args) == -1)
+                {
+                    perror("Failed to execute, command is invalid.");
+                }
+                return 1;
             }
-			return 1;
-		}
-		else{
-			printf("Invalid option. Use -a to display all files within the current directory and its subdirectories.\n");
-		}
-	}
+            else
+            {
+                printf("Invalid option. Use -a to display all files within the current directory and its subdirectories.\n");
+            }
+        }
+    }
 
     // print out all the contents of the directory using opendir() function
     DIR *d;
@@ -35,14 +42,15 @@ int shellListDir_code(char** args)
         }
         closedir(d);
     }
-    else{
-    	printf("Directory doesn't exist. \n");
+    else
+    {
+        printf("Directory doesn't exist. \n");
     }
 
     return 1;
- 
 }
 
-int main(int argc, char** args){
+int main(int argc, char **args)
+{
     return shellListDir_code(args);
 }
